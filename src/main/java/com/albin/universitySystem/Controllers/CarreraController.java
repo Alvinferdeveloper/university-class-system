dda76e12660cd58477b3d7bd73fccbd0fccc80c6
Albin <alvinfer67@gmail.com>
2025-04-24 20:52:53 -0600

    refactor: endpoints to v1

--- src/main/java/com/albin/universitySystem/Controllers/CarreraController.java
+++ src/main/java/com/albin/universitySystem/Controllers/CarreraController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/carrera")
+@RequestMapping("/api/v1/carrera")
 @RequiredArgsConstructor
 public class CarreraController {
     private final CarreraService carreraService;
--- src/main/java/com/albin/universitySystem/Controllers/ComponenteController.java
+++ src/main/java/com/albin/universitySystem/Controllers/ComponenteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/componente")
+@RequestMapping("/api/v1/componente")
 @AllArgsConstructor
 public class ComponenteController {
     private final ComponenteService componenteService;
